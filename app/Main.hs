a249ddb4a0175955f19ae4dfb0bec79541ee13a1
Akash Agrawal <akagr@outlook.com>
2017-01-16 18:04:26 +0530

    Use filename from command line argument

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,16 @@
 module Main where
 
 import Lib as L
+import System.Environment as Env
 import Text.Megaparsec as M
 
-fileName :: String
-fileName = "./input.md"
+getFileName :: [String] -> String
+getFileName [] = ""
+getFileName (x:_) = x
 
 main :: IO ()
 main = do
+  fileName <- fmap getFileName Env.getArgs
   input <- readFile fileName
   putStrLn . toString $ L.parseAll fileName input where
     toString :: Either (ParseError Char Dec) String -> String
